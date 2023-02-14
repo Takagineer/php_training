40d355fc8cd81bef0a9089271603013d106c5691
Takagineer <itengineer0415@gmail.com>
2023-02-15 08:06:59 +0900

    udpate/if文2

---
+++ 2-2.php
@@ -0,0 +1,12 @@
+<?php
+
+echo '数字を入力してください';
+$number = trim(fgets(STDIN));
+
+if ($number > 10) {
+  echo '10より大きいです';
+} elseif ($number == 10) {
+  echo '10です';
+} else {
+  echo '10より小さいです';
+}
